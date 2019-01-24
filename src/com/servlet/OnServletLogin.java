7b3a1d205f9f0cd582982df8165b2350996e48b7
Swetha S <46739649+13swetha@users.noreply.github.com>
2019-01-24 11:11:37 +0530

    bank uploaded

---
+++ src/com/servlet/OnServletLogin.java
@@ -0,0 +1,35 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/login")
+
+public class OnServletLogin extends HttpServlet
+{
+	
+      protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException 
+	{
+    	  PrintWriter pw= res.getWriter();
+  		res.setContentType("text/html");
+  		String user=req.getParameter("userName");
+  		String pass=req.getParameter("userPassword");
+  		if(user.equals("hai")&&pass.equals("hello"))
+  		{
+  			RequestDispatcher dispatcher =req.getRequestDispatcher("a_home.html");
+  			dispatcher.forward(req, res);
+  		}
+  		else
+  		{
+  			pw.println("Login has failed");
+  		}
+  		pw.close();
+	}
+
+}
